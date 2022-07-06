03eff4a74d23d35202c21d92a12d1c32a089ceeb
Fereydoun Memarzanjany <Thraetaona@outlook.com>
2022-07-06 22:55:01 +0300

    Update scanner.py

--- utils/scanner.py
+++ utils/scanner.py
@@ -1,19 +1,17 @@
-#
 # This file is part of AIMER.
-# Unpublished Copyright (C) 2022  Fereydoun Memarzanjany ("AUTHOR"), All Rights Reserved.
+# 
+# AIMER is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# 
+# AIMER is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
 #
-# NOTICE: All information contained herein is, and remains the property of AUTHOR.  The intellectual and technical concepts contained herein are 
-# proprietary to AUTHOR and may be covered by U.S. and Foreign Patents, patents in process, and are protected by trade secret or copyright law.
-# Dissemination of this information or reproduction of this material is strictly forbidden unless prior written permission is obtained from AUTHOR.
-# Access to the source code contained herein is hereby forbidden to anyone except current AUTHOR employees, managers or contractors who have executed 
-# Confidentiality and Non-disclosure agreements explicitly covering such access.
-#
-# The copyright notice above does not evidence any actual or intended publication or disclosure of this source code, which includes information
-# that is confidential and/or proprietary, and is a trade secret, of AUTHOR.  ANY REPRODUCTION, MODIFICATION, DISTRIBUTION, PUBLIC PERFORMANCE, 
-# OR PUBLIC DISPLAY OF OR THROUGH USE OF THIS SOURCE CODE WITHOUT THE EXPRESS WRITTEN CONSENT OF AUTHOR IS STRICTLY PROHIBITED, AND IN VIOLATION OF 
-# APPLICABLE LAWS AND INTERNATIONAL TREATIES.  THE RECEIPT OR POSSESSION OF THIS SOURCE CODE AND/OR RELATED INFORMATION DOES NOT CONVEY OR IMPLY ANY 
-# RIGHTS TO REPRODUCE, DISCLOSE OR DISTRIBUTE ITS CONTENTS, OR TO MANUFACTURE, USE, OR SELL ANYTHING THAT IT MAY DESCRIBE, IN WHOLE OR IN PART.
-#
+# You should have received a copy of the GNU General Public License
+# along with AIMER.  If not, see <https://www.gnu.org/licenses/>.
 
 import cv2 as cv
 import numpy as np
